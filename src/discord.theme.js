be4afb95717edbf570b1348083b58dee0dea75de
malted <bendixon22@protonmail.com>
2021-12-13 02:16:27 +0000

    Initial commit

---
+++ src/discord.theme.js
@@ -0,0 +1,20 @@
+const discord = {
+  blurple: "#5865F2",
+  green: "#57F287",
+  yellow: "#FEE75C",
+  fuchsia: "#EB459E",
+  red: "#ED4245",
+  white: "#FFFFFF",
+
+  greyple: "#99aab5",
+  darkNotBlack: "#2c2f33",
+  offWhite: "#f6f6f6",
+  notQuiteBlack: "#23272a",
+
+  border: "#373A3D",
+  darkest: "#202225",
+  darker: "#2F3136",
+  dark: "#36393F",
+  blurpleLight: "#6370F4",
+  blurpleDark: "#3C45A5",
+};
